b0b09eb17437d44493858323eb08e44d2954c0b5
Nicolas Fernandez <nicolasnahuel.pr@gmail.com>
2020-11-18 15:34:38 -0300

    hice el preguntasFrecuentes y home

--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
-let homePage = require('./homePage')
-
-let movies = homePage.leerJSON()
+const homePage = require('./homePage');
+const FQ = require('./preguntasFrecuentes');
 
 module.exports = {
     homePage : function(req,res){
-        movies.movies.forEach(movie => {
-            res.write(movie.title +'\n')
+        const {total_movies} = homePage.leerJSON();
+
+        res.write(`=============== Titulo: ${homePage.title} ==================\n`)
+        res.write('\n')
+        res.write(`=============== Cantidad peliculas: ${total_movies} ==================\n`)
+        res.write('\n')
+        res.write('===============Peliculas Ordenadas====================\n')
+
+        const orderedMovies = homePage.orderedMovies();
+        orderedMovies.movies.forEach((movie, i) => {
+            res.write(`${i} - ${movie.title} \n `)
         });
-        
-        res.end()
+        res.write('\n')
+
+        res.write('=============== Recordá que podés visitar las secciones : \n -En Cartelera \n -Más Votadas \n -Sucursales \n -Contacto \n -Preguntas frecuentes \n =============== ')
+  
+        res.end();
+    
     },
-    enCartelera : function(req,res){
+    preguntasFrecuentes : function(req,res){
+
+        const {total_faqs} = FQ.leerJSON();
+
+        res.write(`=============== Titulo: ${FQ.title} ==================\n`)
+        res.write('\n')
+        res.write(`=============== Cantidad preguntas: ${total_faqs} ==================\n`)
+        res.write('\n')
+        res.write('=============== Todas las preguntas ====================\n')
+
+        FQ.listOfQuestions().forEach((p, i) => {
+            res.write(`${i}) Title: ${p.faq_title} \n `)
+            res.write(`Reply: ${p.faq_answer} \n \n`)
+            res.write(`-------- \n`)
+        });
+
+        res.end();
 
     }
 }
