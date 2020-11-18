0779f0971a03ed50aa8086a90ec47a79c808f1dc
lucas-damian <lucasherlan99@gmail.com>
2020-11-18 13:31:54 -0300

    comit lucas

---
+++ src/sucursales.js
@@ -0,0 +1,13 @@
+const require =("./src./
+index")
+
+res.write('------------------\n')
+            res.write('NUESTRAS SALAS');
+            res.write('\n**************\n')
+            res.write('\n')
+            res.write(`Total de salas: ${salas.length}` );
+            res.write('\n------------------')
+            res.write('\n\n')
+            salas.forEach(funcion(sucursales)){
+                
+            res.write(`${salas.lenght}`)}
